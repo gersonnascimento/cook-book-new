454d03fb4eec024943754308b8d5599c3a4b9728
gersonnascimento <gerson.nascimento@locaweb.com.br>
2017-12-28 23:37:02 -0200

    form

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,5 +1,14 @@
 class RecipesController < ApplicationController
-	def show
-		@arecipe = Recipe.find(params[:id])
-	end
+  def show
+    @arecipe = Recipe.find(params[:id])
+  end
+
+  def new
+    @new_recipe = Recipe.new
+  end
+
+  def create
+    recipe = Recipe.create(title: params[:recipe][:title], recipe_type: params[:recipe][:recipe_type], cuisine: params[:recipe][:cuisine], difficulty: params[:recipe][:difficulty], cook_time: params[:recipe][:cook_time], ingredients: params[:recipe][:ingredients], method: params[:recipe][:method])
+    redirect_to recipe_path(recipe.id)
+  end
 end
