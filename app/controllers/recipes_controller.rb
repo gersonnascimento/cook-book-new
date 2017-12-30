70da2a3f6b3451bb562d5f819f5d5a4fa91d096d
gersonnascimento <gerson.nascimento@locaweb.com.br>
2017-12-30 21:52:58 -0200

    editrecipe

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -8,11 +8,22 @@ class RecipesController < ApplicationController
   end
 
   def create
-    recipe = Recipe.new(title: params[:recipe][:title], recipe_type_id: params[:recipe][:recipe_type_id], cuisine_id: params[:recipe][:cuisine_id], difficulty: params[:recipe][:difficulty], cook_time: params[:recipe][:cook_time], ingredients: params[:recipe][:ingredients], method: params[:recipe][:method])
+    recipe = Recipe.create(title: params[:recipe][:title], recipe_type_id: params[:recipe][:recipe_type_id], cuisine_id: params[:recipe][:cuisine_id], difficulty: params[:recipe][:difficulty], cook_time: params[:recipe][:cook_time], ingredients: params[:recipe][:ingredients], method: params[:recipe][:method])
     if recipe.save
       redirect_to recipe_path(recipe.id)
     else 
       render 'error_recipe'
+    end
+  end
+  def edit
+   @recipe = Recipe.find(params[:id]) 
+  end
+  def update
+    @recipe = Recipe.find(params[:id])
+    if @recipe.update(title: params[:recipe][:title], recipe_type_id: params[:recipe][:recipe_type_id], cuisine_id: params[:recipe][:cuisine_id], difficulty: params[:recipe][:difficulty], cook_time: params[:recipe][:cook_time], ingredients: params[:recipe][:ingredients], method: params[:recipe][:method])
+    redirect_to recipe_path(@recipe.id)
+    else
+    render 'error_recipe'
     end
   end
 end
