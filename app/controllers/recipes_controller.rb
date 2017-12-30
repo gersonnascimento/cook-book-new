ceaaf7ce35bad6921fe319c3cf478894cab0ca08
gersonnascimento <gerson.nascimento@locaweb.com.br>
2017-12-30 15:30:14 -0200

    foi

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -8,7 +8,11 @@ class RecipesController < ApplicationController
   end
 
   def create
-    recipe = Recipe.create(title: params[:recipe][:title], recipe_type: params[:recipe][:recipe_type], cuisine_id: params[:recipe][:cuisine_id], difficulty: params[:recipe][:difficulty], cook_time: params[:recipe][:cook_time], ingredients: params[:recipe][:ingredients], method: params[:recipe][:method])
-    redirect_to recipe_path(recipe.id)
+    recipe = Recipe.new(title: params[:recipe][:title], recipe_type: params[:recipe][:recipe_type], cuisine_id: params[:recipe][:cuisine_id], difficulty: params[:recipe][:difficulty], cook_time: params[:recipe][:cook_time], ingredients: params[:recipe][:ingredients], method: params[:recipe][:method])
+    if recipe.save
+      redirect_to recipe_path(recipe.id)
+    else 
+      render 'error_recipe'
+    end
   end
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,3 +1,4 @@
 class Recipe < ApplicationRecord
+  validates :title, :recipe_type, :difficulty, :cook_time, :ingredients, :method, presence:true
   belongs_to :cuisine
 end
