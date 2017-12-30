8b62ed43d46fd15c47f379fcd851116165fb07e2
gersonnascimento <gerson.nascimento@locaweb.com.br>
2017-12-30 14:56:21 -0200

    add_cuisine

--- app/controllers/cuisines_controller.rb
+++ app/controllers/cuisines_controller.rb
@@ -3,4 +3,15 @@ class CuisinesController < ApplicationController
     @cuisine = Cuisine.find(params[:id])
     @recipes = Recipe.where("cuisine_id = #{params[:id]}")
   end
+  def new
+    @cuisine = Cuisine.new
+  end
+  def create
+    cuisine = Cuisine.new(name: params[:cuisine][:name])
+    if cuisine.save
+    redirect_to cuisine_path(cuisine.id)
+    else
+    render 'error_cuisine'
+    end
+  end
 end
