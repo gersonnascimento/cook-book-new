4f5abfac8eaf1be67623dff2daa26d3e78eaaf3e
gersonnascimento <gerson.nascimento@locaweb.com.br>
2017-12-30 21:00:59 -0200

    cadastrotiposreceita

--- app/controllers/recipe_types_controller.rb
+++ app/controllers/recipe_types_controller.rb
@@ -3,4 +3,15 @@ class RecipeTypesController < ApplicationController
     @type = RecipeType.find(params[:id])
     @recipes = Recipe.where("recipe_type_id = #{@type.id}")
   end
+  def new
+    @new_type = RecipeType.new
+  end
+  def create
+    type = RecipeType.new(name: params[:recipe_type][:name])
+    if type.save
+      redirect_to recipe_type_path(type.id)
+    else
+      render 'error_type'
+    end
+  end
 end
