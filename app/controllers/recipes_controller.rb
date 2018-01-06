ff9f02d3b8ea1ee09cc78e84c72c7f8bbd476f34
gersonnascimento <gerson.nascimento@locaweb.com.br>
2018-01-06 02:38:54 -0200

    frontboot

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,13 +1,19 @@
 class RecipesController < ApplicationController
   def show
     @arecipe = Recipe.find(params[:id])
+    @cuisines = Cuisine.all
+    @recipe_types = RecipeType.all
   end
 
   def new
-    @new_recipe = Recipe.new
+    @recipe = Recipe.new
+    @cuisines = Cuisine.all
+    @recipe_types = RecipeType.all
   end
 
   def create
+    @cuisines = Cuisine.all
+    @recipe_types = RecipeType.all
     recipe = Recipe.create(title: params[:recipe][:title], recipe_type_id: params[:recipe][:recipe_type_id], cuisine_id: params[:recipe][:cuisine_id], difficulty: params[:recipe][:difficulty], cook_time: params[:recipe][:cook_time], ingredients: params[:recipe][:ingredients], method: params[:recipe][:method])
     if recipe.save
       redirect_to recipe_path(recipe.id)
@@ -16,6 +22,8 @@ class RecipesController < ApplicationController
     end
   end
   def edit
+   @cuisines = Cuisine.all
+   @recipe_types = RecipeType.all
    @recipe = Recipe.find(params[:id]) 
   end
   def update
@@ -27,6 +35,8 @@ class RecipesController < ApplicationController
     end
   end
   def search 
+    @cuisines = Cuisine.all
+    @recipe_types = RecipeType.all
     @word = params[:word]
     @recipes = Recipe.where("title = '#{@word}'")
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root to: "home#index"
   get '/search', to: "recipes#search"
-  resources :recipe_types
-  resources :cuisines
-  resources :recipes
+  resources :recipe_types, only: [:new, :create, :show]
+  resources :cuisines, only: [:new, :create, :show]
+  resources :recipes, only: [:index, :new, :create, :edit, :update, :show]
 end
