8e6abc5106890fefba796bbec014635b3b71ae2c
gersonsnasc <gersonsilvadonascimento@hotmail.com>
2018-01-15 23:39:14 -0400

    foi

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,13 +2,7 @@ class HomeController < ApplicationController
   def index
   	@recipes = Recipe.last(6)
 	if user_signed_in?
-	  @favorites = Favorite.where(user_id: current_user.id)
-	  @frecipes = []	
-	  if @favorites
-	  @favorites.each do |fav|
-            @frecipes << Recipe.find(fav.recipe_id)
-	  end	
-	  end
+  	@favorites = current_user.recipes
+	end
 	end
   end
-end
