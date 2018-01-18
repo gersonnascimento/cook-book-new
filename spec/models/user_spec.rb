4b35655aa94e5feef93c7f58f28e01d433a4d630
gersonsnasc <gersonsilvadonascimento@hotmail.com>
2018-01-18 01:52:44 -0400

    favorited?

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  "add some examples to (or delete) #{__FILE__}"
+  context 'Favoriting  recipes' do
+   it 'user favorited recipe' do
+     user = create(:user)
+     recipe = create(:recipe, user: user)
+     favorite = create(:favorite, user: user, recipe: recipe)
+
+     expect(user.favorited? recipe).to eq(true)
+   end
+   it 'user not favorited recipe' do
+     user = create(:user)
+     recipe = create(:recipe, user: user)
+
+     expect(user.favorited? recipe).to eq(false)
+   end
+  end
 end
