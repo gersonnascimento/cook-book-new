dc3084d92d82a837cabc8fc07d7f0929fb3e329c
gersonsnasc <gersonsilvadonascimento@hotmail.com>
2018-01-19 02:38:19 -0400

    foi

--- spec/features/user_star_recipe_spec.rb
+++ spec/features/user_star_recipe_spec.rb
@@ -6,11 +6,31 @@ feature 'User star recipe' do
     recipe = create(:recipe, user: user)
 
     login_as user
-    visit root_path
-    click_on recipe.title
+    visit recipe_path(recipe)
     click_on 'Star'
 
     expect(page).to have_content('Unstar')
     expect(page).to have_content('Receita marcada com sucesso')
+  end
+
+  scenario 'recipe stared appear different at home' do
+    user = create(:user)
+    recipe = create(:recipe)
+
+    login_as user
+    visit recipe_path(recipe)
+    click_on 'Star'
+    visit root_path
+
+    expect(page).to have_content('(stared)')
+    expect(page).not_to have_link('Star')
+  end
+
+  scenario 'only logge users can star recipes' do
+    recipe = create(:recipe)
+
+    visit recipe_path(recipe)
+
+    expect(page).not_to have_link 'Star'
   end
 end
