3cb647cd383e180a6b5683d876916d0e228b6eff
gersonsnasc <gersonsilvadonascimento@hotmail.com>
2018-01-22 02:20:21 -0400

    picture

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,7 +1,7 @@
 class Recipe < ApplicationRecord
-  attr_accessor :picture_file_name
-  has_attached_file :picture, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/thumb/noimage.jpeg"
-  validates_attachment_file_name :picture, :matches => [/png\Z/, /jpeg\Z/]
+  has_attached_file :picture, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  validates_attachment_content_type :picture, content_type: /\Aimage\/.*\z/
+
   validates :title, :ingredients, :method, presence:true
   validates :difficulty, inclusion: { in: %w(fácil médio difícil facil medio dificil Fácil Médio Difícil Facil Medio Dificil)}
   belongs_to :cuisine
