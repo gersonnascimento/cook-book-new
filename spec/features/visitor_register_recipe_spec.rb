03aa2dc1b313663d726270f965cab26cf1a569c2
gersonsnasc <gersonsilvadonascimento@hotmail.com>
2018-01-23 21:46:10 -0400

    mais um teste de upload

--- spec/features/visitor_register_recipe_spec.rb
+++ spec/features/visitor_register_recipe_spec.rb
@@ -105,6 +105,27 @@ feature 'Visitor register recipe' do
     expect(page).to have_css("img[src*='imagemteste.png']")
   end
 
+  scenario 'user upload picture' do
+    create_user
+    Cuisine.create(name: 'Arabe')
+    RecipeType.create(name: 'Entrada')
+    # simula a ação do usuário
+    visit root_path
+    click_on 'Enviar uma receita'
+
+    fill_in 'Título', with: 'Tabule'
+    select 'Arabe', from: 'Cozinha'
+    select 'Entrada', from: 'Tipo da Receita'
+    fill_in 'Dificuldade', with: 'Fácil'
+    fill_in 'Tempo de Preparo', with: '45'
+    fill_in 'Ingredientes', with: 'Trigo para quibe, cebola, tomate picado, azeite, salsinha'
+    fill_in 'Como Preparar', with: 'Misturar tudo e servir. Adicione limão a gosto.'
+    page.attach_file('Imagem', './spec/support/fixtures/imagemteste.png')
+    click_on 'Enviar'
+
+    expect(page).to have_css("img[src*='imagemteste.png']")
+  end
+
 end
 def create_user
   visit new_user_registration_path
